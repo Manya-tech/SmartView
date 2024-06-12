325f65693e18e7e3437798ba4daab590d99fcbed
Manya-tech <mgmanya2311@gmail.com>
2024-06-12 13:39:09 +0530

    fix: function and variable names

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from predict import predict_sentiment
-from youtube import get_video_info
-from video import get_video_ids
+from youtube import get_video_comments
+from video import get_video
 from flask_cors import CORS
 import pathlib
 import textwrap
@@ -16,28 +16,22 @@ app = Flask(__name__)
 CORS(app)
 
 
-def get_gemini_response(comments,points):
-    model = genai.GenerativeModel('gemini-pro')
-    prompt = f""" You will get a list of comments of a YouTube video as input and the the main points the user wants in that video. 
-Your role is to analyze the comments and create a summary that first mentions how many points the user wants match the video, then what are the main points discussed in a video and 
-what are main themes in the comments. Each heading should have atmost 5 points. Just provide a brief overview that will help the reader to uderstand whether the video has what it wants or what the commentators thought about the video.
-For example:
-Points matching : mention points that matched in bullet point form.
-Viewer insights : mention what people think about the video in bullet point form.
-Main points: mention the main points discussed in the video in bullet point form.
-The points are: {points}. The comments are: {comments}
-"""
-    response = model.generate_content(prompt)
-    result=response.text
-    # result = result[0]['text']
-    return result
+# def get_gemini_response(comments):
+#     model = genai.GenerativeModel('gemini-pro')
+#     prompt = f""" You will get a list of comments of a YouTube video as input. You need to analyze them and mention the top three positive and negative comments.
+#     The comments are: {comments}
+# """
+#     response = model.generate_content(prompt)
+#     result=response.text
+#     return result
 
 
 
-def get_video(video_id, points):
+def get_stats(video):
+    video_id = video['video_id']
     if not video_id:
         return {"error" : "video_id is required"}
-    name, comments = get_video_info(video_id)
+    comments = get_video_comments(video_id)
     print("got info")
     predictions  = predict_sentiment(comments)
     print("predicted")
@@ -45,20 +39,23 @@ def get_video(video_id, points):
     positive = predictions.count("Positive")  
     negative = predictions.count("Negative")
     comments_data = list(zip(comments[:10], predictions[:10]))
-    comms = " "
-    for comment in comments:
-        comms = comms+comment
-    genout = get_gemini_response(comms,points)
+    # comms = " "
+    # for comment in comments:
+    #     comms = comms+comment
+    # genout = get_gemini_response(comms)
 
     summary = {
-        "name" : name,
+        "title" : video['title'],
+        "channel_name" : video['channel_name'],
+        "comment_count" : video['comment_count'],
+        "view_count" : video['view_count'],
+        "like_count" : video['like_count'],
         "video_id" : video_id,
         "positive" : positive,
         "negative" : negative,
         "num_comments" : len(comments),
         "rating" : round((positive/len(comments))*100, 2),
         "comments_data" : comments_data
-        # "genout" : genout
     }
 
     return summary
@@ -68,13 +65,13 @@ def get_video(video_id, points):
 def index():
     data = []
     if request.method == "POST":
-        video_ids = get_video_ids(request.form.get("video_url"))
-        points = request.form.get("points")
-        # video_urls = request.form.get("video_url")
-        # video_urls = video_urls.split(",")
-        for video in video_ids:
-            video_id = video['video_id']
-            res = get_video(video_id,points)
+        video_topic = request.form.get("video_topic")
+        print(video_topic)
+        # points = request.form.get("points")
+        videos_info = get_video(video_topic)
+        for video in videos_info:
+            # video_id = video['video_id']
+            res = get_stats(video)
             print("sent req")
             data.append(res)
 
